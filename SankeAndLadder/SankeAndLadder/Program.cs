908bb21798d66672baf53e12f6590ef94b2c1430
sayalikeche <kechesayali11@gmail.com>
2021-11-14 22:00:52 +0530

    [sayali Adding Snake And Ladder]

---
+++ SankeAndLadder/SankeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SankeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Sanke And Ladder");
+            Console.ReadLine();
+        }
+    }
+}
